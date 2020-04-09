cb07fca0a3440aa1b4cf04d40b9c738375847d88
SeverinW <severin1.will@stud.uni-r.de>
2020-04-09 13:09:07 +0200

    Lösungsvorschlag ergänzt

--- app/src/main/java/de/ur/mi/android/excercises/walkthrough/PageOne.java
+++ app/src/main/java/de/ur/mi/android/excercises/walkthrough/PageOne.java
@@ -1,25 +1,44 @@
-package de.ur.mi.android.excercises.walkthrough;
+package de.ur.mi.android.exercises.walkthrough;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class PageOne extends Activity {
+public class PageOne extends Activity implements View.OnClickListener {
 
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_walkthrough);
+    protected Button buttonBack;
+    protected Button buttonNext;
+    protected TextView textView;
 
-   }
 
-   @Override
-   public boolean onCreateOptionsMenu(Menu menu) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_walkthrough);
+        buttonBack = findViewById(R.id.button_back);
+        buttonNext = findViewById(R.id.button_next);
+        textView = findViewById(R.id.textView);
+        textView.setText(R.string.page_one);
+        buttonBack.setEnabled(false);
+        buttonNext.setOnClickListener(this);
+        buttonBack.setOnClickListener(this);
+    }
 
-      // Inflate the menu; this adds items to the action bar if it is present.
-      getMenuInflater().inflate(R.menu.page_one, menu);
-      return true;
-   }
+    @Override
+    public void onClick(View v) {
+        buttonNextClicked();
+    }
+
+    private void buttonNextClicked(){
+        Intent intent = new Intent(PageOne.this, PageTwo.class);
+        startActivity(intent);
+    }
 
 
 }
+
+
+
+
