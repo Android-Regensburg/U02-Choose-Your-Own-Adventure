0182eda81430c9d4faaa6cf3ebef9d97c557cde2
Alexander Bazo <alexanderbazo@googlemail.com>
2022-04-28 15:51:15 +0200

    Update for new semester

--- app/src/main/java/de/ur/mi/android/chooseyourownadventure/BaseActivity.java
+++ app/src/main/java/de/ur/mi/android/chooseyourownadventure/BaseActivity.java
@@ -1,19 +1,19 @@
 package de.ur.mi.android.chooseyourownadventure;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * Die BaseActivity dient als Vorlage (Superklasse) für alle Activities der Anwendung. Hier werden
  * die gemeinsamen Eigenschaften modelliert, die in den übrigen Activities zur Darstellung des
  * Kapiteltexts und zum Abfangen der NutzerInnen-Interaktion notwendig sind.
  */
-public class BaseActivity extends Activity implements View.OnClickListener {
+public class BaseActivity extends AppCompatActivity {
 
     // Instanzvariablen für Referenzen auf UI-Elemente
     protected Button optionA;
@@ -31,17 +31,22 @@ public class BaseActivity extends Activity implements View.OnClickListener {
     /**
      * In dieser Methode werden die Referenzen auf die notwendigen UI-Elemente erstellt und in
      * den entsprechenden Instanzvariablen abgespeichert. Der Klick auf die Buttons wird über das
-     * Setzen entsprechender Listener realisiert. Die Activity selbst implementiert dazu das
-     * notwendige Interface (View.OnClickListener) und kann daher als Ziel für die Events verwendet
-     * werden. Als Callback-Methode dient dabei die, durch das Implementieren des Interface vorgegebene,
-     * Methode onCLick.
+     * Setzen entsprechender Listener realisiert. Als engültige Callback-Methode dient dabei die,
+     * in dieser Activity definierte, handleCLickOnView-Methode.
      */
     private void initUI() {
         setContentView(R.layout.activity_walkthrough);
         optionA = findViewById(R.id.buttonOptionA);
         optionB = findViewById(R.id.buttonOptionB);
-        optionA.setOnClickListener(this);
-        optionB.setOnClickListener(this);
+        optionA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleClickOnView(view);
+            }
+        });
+        // Kürzer geht es mit einer Lambda-Funktion: optionA.setOnClickListener(view -> handleClickOnView(view));
+        // Noch kürzer gehts mit einer Methoden-Referenz: optionA.setOnClickListener(this::handleClickOnView);
+        optionB.setOnClickListener(this::handleClickOnView);
         textContent = findViewById(R.id.content);
     }
 
@@ -51,8 +56,7 @@ public class BaseActivity extends Activity implements View.OnClickListener {
      * Methode, um diese Klasse über die Klick-Events zu informieren. Beim Aufruf wird aber jeweils
      * die konkrete Button-Instanz als Parameter (v) übergeben.
      */
-    @Override
-    public void onClick(View v) {
+    private void handleClickOnView(View v) {
         int id = v.getId();
         if (id == R.id.buttonOptionA) {
             startIntent(targetForOptionA);
@@ -66,7 +70,7 @@ public class BaseActivity extends Activity implements View.OnClickListener {
      * Activity, die als target-Parameter übergebene Activity aufruft.
      */
     private void startIntent(Class target) {
-        /**
+        /*
          * Um in den abgeleiteten Subklassen den korrekten Kontext für den Intent zu verwenden, wird
          * an dieser Stelle nicht der aktulle Context hart eincodiert (BaseActvity.this) sondern bei
          * jedem Aufruf dynamsich ausgelesend (this.getBaseContext()). Dadurch wird der Intent immer
